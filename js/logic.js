0d44bf7ffe0f70eed64ce9db71b718ebb9d60b15
jiang li <jiangl.li@emerson.com>
2018-02-06 17:51:45 +0800

    Add validations, support LCL and fixing some issues

--- js/logic.js
+++ js/logic.js
@@ -17,6 +17,40 @@ function initControlListeners() {
   
   $('#portOfLoading').keyup(getCurrencyUpdater('orig', 'portOfLoading', 'oCurrLabel', 'oCurrValue'));
   $('#portOfDischarge').keyup(getCurrencyUpdater('dest', 'portOfDischarge', 'dCurrLabel', 'dCurrValue'));
+  
+  $('#fcl').change(changeCL);
+  $('#lcl').change(changeCL);
+  
+  changeCL();
+}
+
+function changeCL() {
+  var fclSelected = $('#fcl').get(0).checked;
+  enableDiableCLControls(fclSelected);
+}
+
+function enableDiableCLControls(fclSelected) {
+  if (fclSelected) {
+    $('#numOf20GP').attr("disabled",false);
+    $('#numOf40GP').attr("disabled",false); 
+    $('#numOf40HQ').attr("disabled",false); 
+    
+    $('#grossWeight').attr("disabled",true); 
+    $('#volume').attr("disabled",true);
+    $('#grossWeight').val(0); 
+    $('#volume').val(0);  
+  } else {
+    $('#numOf20GP').attr("disabled",true);
+    $('#numOf40GP').attr("disabled",true); 
+    $('#numOf40HQ').attr("disabled",true); 
+    $('#numOf20GP').val(0);
+    $('#numOf40GP').val(0);
+    $('#numOf40HQ').val(0);
+    
+    $('#grossWeight').attr("disabled",false); 
+    $('#volume').attr("disabled",false); 
+  }
+  $("#result")[0].innerHTML = "";
 }
 
 function getCurrencyUpdater(portCategory, portId, currencyLabelId, currencyValueId) {
@@ -25,6 +59,9 @@ function getCurrencyUpdater(portCategory, portId, currencyLabelId, currencyValue
     var formatedPort = tostr(port);
     laneInfo = null;
     var portAttr = (portCategory == 'orig' ? 'loadPort' : 'dischargePort');
+    
+    var fclSelected = $('#fcl').get(0).checked;
+    var lanes = fclSelected ? FCL_LANES : LCL_LANES;
     for (var i = 0; i < lanes.length; i++) {
       if (formatedPort == lanes[i].lane[portAttr]) {
         laneInfo = lanes[i];
@@ -73,23 +110,43 @@ function handleSubmit() {
   });
 }
 
-function calculate() {
-  var loadPort = $('#portOfLoading').val();
-  var dischargePort = $('#portOfDischarge').val();
+
+var loadPort = null;
+var dischargePort = null;
+var formatedLoadPort = null;
+var formatedDischargePort = null;
+
+var num20Gp = null;
+var num40Gp = null;
+var num40Hq = null;
+var origCurrency = null;
+var destCurrency = null;
+
+var grossWeight = null;
+var volume = null;
+
+function calculate() {  
+  loadPort = $('#portOfLoading').val();
+  dischargePort = $('#portOfDischarge').val();
   
-  var formatedLoadPort = tostr(loadPort);
-  var formatedDischargePort = tostr(dischargePort);
+  formatedLoadPort = tostr(loadPort);
+  formatedDischargePort = tostr(dischargePort);
   
-  var num20Gp = $('#numOf20GP').val();
+  num20Gp = $('#numOf20GP').val();
   num20Gp = (num20Gp == '' ? 0 : parseInt(num20Gp));
-  var num40Gp = $('#numOf40GP').val();
+  num40Gp = $('#numOf40GP').val();
   num40Gp = (num40Gp == '' ? 0 : parseInt(num40Gp));
-  var num40Hq = $('#numOf40HQ').val();
+  num40Hq = $('#numOf40HQ').val();
   num40Hq = (num40Hq == '' ? 0 : parseInt(num40Hq));
   
-  var origCurrency = $('#oCurrValue').val();
+  grossWeight = $('#grossWeight').val();
+  grossWeight = (grossWeight == '' ? 0 : parseFloat(grossWeight));
+  volume = $('#volume').val();
+  volume = (volume == '' ? 0 : parseFloat(volume));
+  
+  origCurrency = $('#oCurrValue').val();
   origCurrency = (origCurrency == '' ? 0 : parseFloat(origCurrency));
-  var destCurrency = $('#dCurrValue').val();
+  destCurrency = $('#dCurrValue').val();
   destCurrency = (destCurrency == '' ? 0 : parseFloat(destCurrency));
   
   if (origCurrency == 0) {
@@ -102,13 +159,48 @@ function calculate() {
   if (origCurrency == 0 && destCurrency == 0) {
     return;
   }
-    
+   
+  var fclSelected = $('#fcl').get(0).checked;
+  if (fclSelected) {
+    calculateFCLCost();
+  } else {
+    calculateLCLCost();
+  }
+}
+
+function getLaneToCalculate(odLanes, city, isOrigin) {
+  var cityAttr = isOrigin ? "oc" : "dc";
+  var portAttr = isOrigin ? "loadPort" : "dischargePort";
+  var citySameAsPortLane = null;
+  for (var i = 0; i < odLanes.length; i++) {
+    var odLane = odLanes[i];
+    if (city == odLane.lane[cityAttr]) {
+      return odLane;
+    }
+    if (citySameAsPortLane == null && odLane.lane[cityAttr] == odLane.lane[portAttr]) {
+      citySameAsPortLane = odLane;
+    }
+  }
+  
+  return citySameAsPortLane;
+}
+
+
+/**
+ * Calculate the FCL cost logics begin here.
+ */
+function calculateFCLCost() {
   var matchedLanes = {size: 0};
-  for (var i = 0; i < lanes.length; i++) {
-    var laneInfo = lanes[i];
+  for (var i = 0; i < FCL_LANES.length; i++) {
+    var laneInfo = FCL_LANES[i];
     if (formatedLoadPort == laneInfo.lane.loadPort && formatedDischargePort == laneInfo.lane.dischargePort) {
-      matchedLanes.size++;
-      matchedLanes[laneInfo.lane.contType] = laneInfo;
+	  var conTypeLanes = matchedLanes[laneInfo.lane.contType];
+	  if (conTypeLanes == null) {
+		  conTypeLanes = [];
+		  matchedLanes[laneInfo.lane.contType] = conTypeLanes; 
+		  matchedLanes.size++;
+	  }
+	  matchedLanes[laneInfo.lane.contType].push(laneInfo);
       matchedLanes.ohc = laneInfo.orig.hc;
       matchedLanes.oecc = laneInfo.orig.ecc;
       matchedLanes.dhc = laneInfo.dest.hc;
@@ -126,11 +218,17 @@ function calculate() {
   var containers = [{key: '20gp', num: num20Gp}, {key: '40gp', num: num40Gp}, {key: '40hq', num: num40Hq}];
   for (var i = 0; i < containers.length; i++) {
     var container = containers[i];
-    var costForContainer = calculateCost(container, matchedLanes[container.key], origCurrency, destCurrency);
-    if (isNaN(parseFloat(costForContainer))) {
-      msg += (costForContainer + '<br/>');
+    var contTypeLanes = matchedLanes[container.key];
+    var calulateMsg = '';
+    if (container.num > 0 && contTypeLanes == null) {
+      msg += "No " + container.key + " Rate for lane from " + loadPort + " to " + dischargePort + '<br/>';
     } else {
-      cost += costForContainer;
+      var costForContainer = doCalculateFCLCost(container, contTypeLanes, origCurrency, destCurrency);
+      if (isNaN(parseFloat(costForContainer))) {
+        msg += (costForContainer + '<br/>');
+      } else {
+        cost += costForContainer;
+      }
     }
   }
   
@@ -159,80 +257,410 @@ function calculate() {
   document.getElementById('result').innerHTML = result;
 }
 
-function calculateCost(container, lane20Gp, origCurrency, destCurrency) {
-  var cost20Gp = 0;
+function doCalculateFCLCost(container, contTypeLanes, origCurrency, destCurrency) {
+  var cost = 0;
   if (!container.num) {
-    return cost20Gp;
+    return cost;
   }
-  if (lane20Gp == null) {
-    return 'No Lane for ' + container.key; 
-  }
+  
+  var oCity = $('#originCity').val();
+  var dCity = $('#destinationCity').val();
+  var otRequired = $('#otRequired').get(0).checked;
+  var dtRequired = $('#dtRequired').get(0).checked;
+  
+  var formatedOcity = tostr(oCity);
+  var formatedDcity = tostr(dCity);
   
   var calculateMsg = '';
-  var originCost = calculateOriginCost(container, lane20Gp, origCurrency);
-  if (isNaN(parseFloat(originCost))) {
-    calculateMsg += (originCost + '<br/>');
-  } else {
-    cost20Gp += originCost;
-  }
-  
-  var destCost = calculateDestinationCost(container, lane20Gp, destCurrency);
-  if (isNaN(parseFloat(destCost))) {
-    calculateMsg += (destCost + '<br/>');
-  } else {
-    cost20Gp += destCost;
-  }
-  
-  var oceanCost = calculateOceanCost(container, lane20Gp);
-  cost20Gp += oceanCost;
-  return calculateMsg == '' ? cost20Gp : calculateMsg;
-}
-
-function calculateOceanCost(container, lane20Gp) {
-  return (lane20Gp.ocean.freignt + lane20Gp.ocean.vgm) * container.num;
-}
-
-function calculateOriginCost(container, lane20Gp, origCurrency) {
-  var oc = $('#originCity').val();
-
-  var otRequried = $('#otRequired').get(0).checked;
-  var oc2PortKM = $('#distanctFromOCity2Loading').val();
-  if (otRequried && oc2PortKM > 100 && oc != lane20Gp.lane.oc) {
-    return 'No origin trucking rate for ' + container.key;
-  }
-  
-  var costOrigin = 0;
-  costOrigin += lane20Gp.orig.thc;
-  if (otRequried) {
-    if (oc2PortKM <= 50) {
-      costOrigin += lane20Gp.orig.tZone1;
-    } else if (oc2PortKM <= 100) {
-      costOrigin += lane20Gp.orig.tZone2;
+  var calculateOrigMsg = '';
+  var origLane = getLaneToCalculate(contTypeLanes, formatedOcity, true);
+  if (origLane == null) {
+    if (otRequired) {
+      calculateOrigMsg = 'No Trucking Rate for ' + container.key + ' from ' + oCity + '<br/>'; 
     } else {
-      costOrigin += lane20Gp.orig.t;
+      origLane = contTypeLanes[0];
     }
+  }
+  
+  if (origLane != null) {
+    var originCost = calculateOriginCost(container, origLane, origCurrency);
+    if (isNaN(parseFloat(originCost))) {
+      calculateOrigMsg += (originCost + '<br/>');
+    } else {
+      cost += originCost;
+    }
+  }
+  calculateMsg = calculateOrigMsg;
+  
+  var calculateDestMsg = '';
+  var destLane = getLaneToCalculate(contTypeLanes, formatedDcity, false);
+  if (destLane == null) {
+    if (dtRequired) {
+      calculateDestMsg = 'No Trucking Rate for ' + container.key + ' to ' + dCity + '<br/>'; 
+    } else {
+      destLane = contTypeLanes[0];
+    }
+  }
+  
+  if (destLane != null) {
+    var destCost = calculateDestinationCost(container, destLane, destCurrency);
+    if (isNaN(parseFloat(destCost))) {
+      calculateDestMsg += (destCost + '<br/>');
+    } else {
+      cost += destCost;
+    }
+    
+    var oceanCost = calculateOceanCost(container, destLane);
+    cost += oceanCost;
+  }
+  calculateMsg += calculateDestMsg;
+  
+  return calculateMsg == '' ? cost : calculateMsg;
+}
+
+function calculateOceanCost(container, lane) {
+  return (lane.ocean.freignt + lane.ocean.vgm) * container.num;
+}
+
+function calculateOriginCost(container, lane, origCurrency) {
+  var oc = tostr($('#originCity').val());
+
+  var otRequired = $('#otRequired').get(0).checked;
+  var oc2PortKM = $('#distanctFromOCity2Loading').val();
+
+  var costOrigin = 0;
+  costOrigin += lane.orig.thc;
+  if(otRequired){
+   if (oc2PortKM <= 50) {
+	  if (lane.orig.tZone1==null){
+	    if (lane.orig.t==null){
+		  return "No Origin Trucking Rate"
+		}else{
+		  if (oc==lane.lane.oc){
+		    costOrigin+=lane.orig.t;
+		  }else{
+		     return "No Origin Trucking Rate"
+		  }
+		}
+	  }else{
+	  costOrigin+=lane.orig.tZone1
+	  }
+   } else if (oc2PortKM<=100) {
+	  if (lane.orig.tZone2==null){
+	    if (lane.orig.t==null){
+		  return "No Origin Trucking Rate"
+		}else{
+		  if (oc==lane.lane.oc){
+		    costOrigin+=lane.orig.t;
+		  }else{
+		     return "No Origin Trucking Rate"
+		  }
+		}
+	  }
+	  else{
+	  costOrigin+=lane.orig.tZone2
+	  } 
+   } else if (oc2PortKM>100) {
+	  if (lane.orig.t==null){
+	    return "No Origin Trucking Rate "
+	  }else{
+	    if(oc==lane.lane.oc){
+		   costOrigin+=lane.orig.t;
+		}else{
+		  return "No Origin Trucking Rate"
+		}
+	  }
+    
+	
+   }
   }
   return  costOrigin * container.num * origCurrency;
+  
 }
 
-function calculateDestinationCost(container, lane20Gp, destCurrency) {
-  var dc = $('#destinationCity').val();
-  var dtRequried = $('#dtRequired').get(0).checked;
+function calculateDestinationCost(container, lane, destCurrency) {
+  var dc = tostr($('#destinationCity').val());
+  var dtRequired = $('#dtRequired').get(0).checked;
   var dc2PortKM = $('#distanctFromDischarge2DCity').val();
-  if (dtRequried && dc2PortKM > 100 && dc != lane20Gp.lane.dc) {
-    return 'No destination trucking rate for ' + container.key;
-  }
-  
+  dc2PortKM = parseFloat(dc2PortKM);
+
   var costDestination = 0;
-  costDestination += lane20Gp.dest.thc;
-  if (dtRequried) {
+  costDestination += lane.dest.thc;
+  if(dtRequired){
+	  
     if (dc2PortKM <= 50) {
-      costDestination += lane20Gp.dest.tZone1;
-    } else if (dc2PortKM <= 100) {
-      costDestination += lane20Gp.dest.tZone2;
-    } else {
-      costDestination += lane20Gp.dest.t;
-    }
+	  if (lane.dest.tZone1==null){
+	    if (lane.dest.t==null){
+		  return "No Destination Trucking Rate"
+		}else{
+		  if (dc==lane.lane.dc){
+		    costDestination+=lane.dest.t;
+		  }else{
+		     return "No Destination Trucking Rate"
+		  }
+		}
+	  }else{
+	  costDestination+=lane.dest.tZone1
+	  }
+	} else if (dc2PortKM<=100) {
+	  if (lane.dest.tZone2==null){
+	    if (lane.dest.t==null){
+		  return "No Destination Trucking Rate"
+		}else{
+		  if (dc==lane.lane.dc){
+		    costDestination+=lane.dest.t;
+		  }else{
+		     return "No Destination Trucking Rate"
+		  }
+		}
+	  }
+	  else{
+	  costDestination+=lane.dest.tZone2
+	  } 
+	} else if (dc2PortKM>100){
+	  if (lane.dest.t==null){
+	    return "No Destination Trucking Rate "
+	  }else{
+	    if(dc==lane.lane.dc){
+		   costDestination+=lane.dest.t;
+		}else{
+		  return "No Destination Trucking Rate"
+		}
+	  }
+	}
+
   }
   return  costDestination * container.num * destCurrency;
-} 
+  
+}
+
+
+/**
+ * Calculate the LCL cost logics begin here.
+ */
+function calculateLCLCost() {
+  var matchedLanes = [];
+  for (var i = 0; i < LCL_LANES.length; i++) {
+    var laneInfo = LCL_LANES[i];
+    if (formatedLoadPort == laneInfo.lane.loadPort && formatedDischargePort == laneInfo.lane.dischargePort) {
+	    matchedLanes.push(laneInfo);
+      matchedLanes.oStandard1 = laneInfo.orig.standard1;
+      matchedLanes.oecc = laneInfo.orig.ecc;
+      matchedLanes.dStandard1 = laneInfo.dest.standard1;
+      matchedLanes.dicc = laneInfo.dest.icc;
+    }
+  }
+  
+  if (matchedLanes.length == 0) {
+    document.getElementById('result').innerHTML = ("No Rate for lane from " + loadPort + " to " + dischargePort);
+    return ;
+  }
+  
+  var cost = doCalculateLCLCost(matchedLanes, origCurrency, destCurrency);
+  var msg = '';
+  if (isNaN(parseFloat(cost))) {
+    msg += (cost + '<br/>');
+  }
+  
+  var eccRequired = $('#eccRequired').get(0).checked;
+  var iccRequried = $('#iccRequired').get(0).checked;
+  var result = '';
+  if (msg == '') {
+    if (grossWeight > 0 && volume > 0) {
+      var origOneTimeFee = matchedLanes.oStandard1;
+      if (eccRequired) {
+        origOneTimeFee += matchedLanes.oecc;
+      }
+      var destOneTimeFee = matchedLanes.dStandard1;
+      if (iccRequried) {
+        destOneTimeFee += matchedLanes.dicc;
+      }
+      
+      cost = (cost + origOneTimeFee * origCurrency);
+      cost = (cost + destOneTimeFee * destCurrency);
+    }
+    result = (cost.toFixed(2) + ' $');
+  } else {
+    result = msg;
+  }
+  
+  document.getElementById('result').innerHTML = result;
+}
+
+function doCalculateLCLCost(contTypeLanes, origCurrency, destCurrency) {
+  var cost = 0;
+  if (grossWeight == 0 || volume == 0) {
+    return cost;
+  }
+  
+  var oCity = $('#originCity').val();
+  var dCity = $('#destinationCity').val();
+  var otRequired = $('#otRequired').get(0).checked;
+  var dtRequired = $('#dtRequired').get(0).checked;
+  
+  var formatedOcity = tostr(oCity);
+  var formatedDcity = tostr(dCity);
+  
+  var calculateMsg = '';
+  var calculateOrigMsg = '';
+  var origLane = getLaneToCalculate(contTypeLanes, formatedOcity, true);
+  if (origLane == null) {
+    if (otRequired) {
+      calculateOrigMsg = 'No Trucking Rate from ' + oCity + '<br/>'; 
+    } else {
+      origLane = contTypeLanes[0];
+    }
+  }
+  
+  if (origLane != null) {
+    var originCost = calculateLCLOriginCost(origLane, origCurrency);
+    if (isNaN(parseFloat(originCost))) {
+      calculateOrigMsg += (originCost + '<br/>');
+    } else {
+      cost += originCost;
+    }
+  }
+  calculateMsg = calculateOrigMsg;
+  
+  var calculateDestMsg = '';
+  var destLane = getLaneToCalculate(contTypeLanes, formatedDcity, false);
+  if (destLane == null) {
+    if (dtRequired) {
+      calculateDestMsg = 'No Trucking Rate to ' + dCity + '<br/>'; 
+    } else {
+      destLane = contTypeLanes[0];
+    }
+  }
+  
+  if (destLane != null) {
+    var destCost = calculateLCLDestinationCost(destLane, destCurrency);
+    if (isNaN(parseFloat(destCost))) {
+      calculateDestMsg += (destCost + '<br/>');
+    } else {
+      cost += destCost;
+    }
+    
+    var oceanCost = calculateLCLOceanCost(destLane);
+    cost += oceanCost;
+  }
+  calculateMsg += calculateDestMsg;
+  
+  return calculateMsg == '' ? cost : calculateMsg;
+}
+
+function calculateLCLOceanCost(lane) {
+  var chargeWeight = Math.max(grossWeight / 1000, volume);
+  return (Math.max(lane.ocean.freignt * chargeWeight, lane.ocean.minFreignt) + lane.ocean.vgm);
+}
+
+function calculateLCLOriginCost(lane, origCurrency) {
+  var oc = tostr($('#originCity').val());
+
+  var otRequired = $('#otRequired').get(0).checked;
+  var oc2PortKM = $('#distanctFromOCity2Loading').val();
+
+  var costOrigin = 0;
+  var chargeWeight = Math.max(grossWeight / 1000, volume);
+  costOrigin += Math.max((lane.orig.standard2 * chargeWeight), lane.orig.minStandard2);
+
+  if (otRequired) {
+    if (oc2PortKM <= 50) {
+      if (lane.orig.tZone1==null){
+        if (lane.orig.t==null){
+          return "No Origin Trucking Rate"
+        }else{
+          if (oc==lane.lane.oc){
+            costOrigin += Math.max(lane.orig.t*chargeWeight, lane.orig.mint);
+          }else{
+             return "No Origin Trucking Rate"
+          }
+        }
+      }else{
+        costOrigin += Math.max(lane.orig.tZone1*chargeWeight, lane.orig.mintZone1);
+      }
+    } else if (oc2PortKM<=100) {
+      if (lane.orig.tZone2==null){
+        if (lane.orig.t==null){
+          return "No Origin Trucking Rate"
+        }else{
+          if (oc==lane.lane.oc){
+            costOrigin += Math.max(lane.orig.t*chargeWeight, lane.orig.mint);
+          }else{
+             return "No Origin Trucking Rate"
+          }
+        }
+      }
+      else{
+        costOrigin += Math.max(lane.orig.tZone2*chargeWeight, lane.orig.mintZone2);
+      } 
+    } else if (oc2PortKM>100) {
+      if (lane.orig.t==null){
+        return "No Origin Trucking Rate "
+      }else{
+        if(oc==lane.lane.oc){
+         costOrigin += Math.max(lane.orig.t*chargeWeight, lane.orig.mint);
+        }else{
+          return "No Origin Trucking Rate"
+        }
+      }
+    }
+  }
+   
+  return costOrigin * origCurrency;
+}
+
+function calculateLCLDestinationCost(lane, destCurrency) {
+  var dc = tostr($('#destinationCity').val());
+  var dtRequired = $('#dtRequired').get(0).checked;
+  var dc2PortKM = $('#distanctFromDischarge2DCity').val();
+  dc2PortKM = parseFloat(dc2PortKM);
+
+  var costDestination = 0;
+  var chargeWeight = Math.max(grossWeight / 1000, volume);
+  costDestination += Math.max((lane.dest.standard2 * chargeWeight), lane.dest.minStandard2);
+  if(dtRequired){
+    if (dc2PortKM <= 50) {
+      if (lane.dest.tZone1==null){
+        if (lane.dest.t==null){
+          return "No Destination Trucking Rate"
+        }else{
+          if (dc==lane.lane.dc){
+            costDestination += Math.max(lane.dest.t*chargeWeight, lane.dest.mint);
+          }else{
+             return "No Destination Trucking Rate"
+          }
+        }
+      }else{
+        costDestination += Math.max(lane.dest.tZone1*chargeWeight, lane.dest.mintZone1);
+      }
+    } else if (dc2PortKM<=100) {
+      if (lane.dest.tZone2==null){
+        if (lane.dest.t==null){
+          return "No Destination Trucking Rate"
+        }else{
+          if (dc==lane.lane.dc){
+            costDestination += Math.max(lane.dest.t*chargeWeight, lane.dest.mint);
+          }else{
+             return "No Destination Trucking Rate"
+          }
+        }
+      }
+      else{
+        costDestination += Math.max(lane.dest.tZone2*chargeWeight, lane.dest.mintZone2);
+      } 
+    } else if (dc2PortKM>100){
+      if (lane.dest.t==null){
+        return "No Destination Trucking Rate "
+      }else{
+        if(dc==lane.lane.dc){
+          costDestination += Math.max(lane.dest.t*chargeWeight, lane.dest.mint);
+        }else{
+          return "No Destination Trucking Rate"
+        }
+      }
+    }
+  }
+  return  costDestination * destCurrency;
+  
+}
+
--- js/xls2data.js
+++ js/xls2data.js
@@ -10,6 +10,12 @@ function Init() {
   filedrag.addEventListener("dragleave", FileDragHover, false);
   filedrag.addEventListener("drop", FileSelectHandler, false);
   filedrag.style.display= "block";
+  
+  var lclFiledrag = $id("lclFiledrag");
+  lclFiledrag.addEventListener("dragover", FileDragHover, false);
+  lclFiledrag.addEventListener("dragleave", FileDragHover, false);
+  lclFiledrag.addEventListener("drop", LCLFileSelectHandler, false);
+  lclFiledrag.style.display= "block";
 }
 
 // file drag hover
@@ -95,5 +101,92 @@ function fillDest(fdata, d) {
   fdata.dest.tZone1 = tonum(d.dtZone1);
   fdata.dest.tZone2 = tonum(d.dtZone2);
   fdata.dest.t = tonum(d.dt);
+  fdata.dest.currency = tostr(d.dCurrency);
+}
+
+
+// file selection
+function LCLFileSelectHandler(e) {
+  // cancel event and hover styling
+  FileDragHover(e);
+  lclHandleDrop(e);
+}
+
+function lclHandleDrop(e) {
+  e.stopPropagation(); e.preventDefault();
+  var files = e.dataTransfer.files, f = files[0];
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    var data = e.target.result;
+    if(!rABS) data = new Uint8Array(data);
+    var workbook = XLSX.read(data, {type: rABS ? 'binary' : 'array'});
+    lclReadFromXsl(workbook);
+    /* DO SOMETHING WITH workbook HERE */
+  };
+  if(rABS) reader.readAsBinaryString(f); else reader.readAsArrayBuffer(f);
+}
+
+function lclReadFromXsl(workbook) {
+  var worksheet = workbook.Sheets[workbook.SheetNames[2]];
+  var data = XLSX.utils.sheet_to_json(worksheet, {range:4, header:[
+    'lot', 'carrier', 'role', 'efcFrom', 'efcTo', 'bp',	'bu', 'oCity', 'loadPort', 'oCountry',
+    'dischargePort', 'dCity', 'dCountry', 'shipmentType', 'serviceMode', 'incoTerm',
+    'oCurrency', 'ecc', 'oStandard1', 'oStandard2', 'minOStandard2', 'otZone1', 'minOtZone1', 'otZone2', 'minOtZone2', 'ot', 'minOt',
+    'freignt', 'minFreignt', 'vgm',	
+    'dCurrency', 'icc', 'dStandard1', 'dStandard2', 'minDStandard2', 'dtZone1', 'minDtZone1', 'dtZone2', 'minDtZone2', 'dt', 'minDt'
+  ]});
+  
+  var fdata = [];
+  for (var i = 0; i < data.length; i++) {
+    var d = data[i];
+    fdata[i] = {lane:{}, orig:{}, ocean:{}, dest:{}};
+    fillLCLLane(fdata[i], d);
+    fillLCLOrig(fdata[i], d);
+    fillLCLOcean(fdata[i], d);
+    fillLCLDest(fdata[i], d);
+  }
+  
+  document.writeln(JSON.stringify(fdata));
+}
+
+function fillLCLLane(fdata, d) {
+  fdata.lane.oc = tostr(d.oCity);
+  fdata.lane.loadPort = tostr(d.loadPort); 
+  fdata.lane.dc = tostr(d.dCity); 
+  fdata.lane.dischargePort = tostr(d.dischargePort);  
+}
+
+function fillLCLOrig(fdata, d) {
+  fdata.orig.ecc = tonum(d.ecc);
+  fdata.orig.standard1 = tonum(d.oStandard1);
+  fdata.orig.standard2 = tonum(d.oStandard2);
+  fdata.orig.minStandard2 = tonum(d.minOStandard2);
+  fdata.orig.tZone1 = tonum(d.otZone1);
+  fdata.orig.mintZone1 = tonum(d.minOtZone1);
+  fdata.orig.tZone2 = tonum(d.otZone2);
+  fdata.orig.mintZone2 = tonum(d.minOtZone2);
+  fdata.orig.t = tonum(d.ot);
+  fdata.orig.mint = tonum(d.minOt);
+  fdata.orig.currency = tostr(d.oCurrency);
+}
+
+function fillLCLOcean(fdata, d) {
+  fdata.ocean.freignt = tonum(d.freignt);
+  fdata.ocean.minFreignt = tonum(d.minFreignt);
+  fdata.ocean.vgm = tonum(d.vgm);
+  fdata.ocean.currency = 'usd';
+}
+
+function fillLCLDest(fdata, d) {
+  fdata.dest.icc = tonum(d.icc);
+  fdata.dest.standard1 = tonum(d.dStandard1);
+  fdata.dest.standard2 = tonum(d.dStandard2);
+  fdata.dest.minStandard2 = tonum(d.minDStandard2);
+  fdata.dest.tZone1 = tonum(d.dtZone1);
+  fdata.dest.mintZone1 = tonum(d.minDtZone1);
+  fdata.dest.tZone2 = tonum(d.dtZone2);
+  fdata.dest.mintZone2 = tonum(d.minDtZone2);
+  fdata.dest.t = tonum(d.dt);
+  fdata.dest.mint = tonum(d.minDt);
   fdata.dest.currency = tostr(d.dCurrency);
 }
